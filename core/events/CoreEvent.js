82b0cdc8eaf74c016cd41830f220e75866d8caf6
Geert Fokke <geert@sector22.com>
2015-06-01 18:13:38 +0200

    Added Event Dispatcher & Event classes. Created a core polyfill file.

---
+++ core/events/CoreEvent.js
@@ -0,0 +1,53 @@
+/**
+ * @author Geert Fokke [geert@sector22.com]
+ * @www sector22.com
+ * @module sector22/core/events
+ */
+
+// @formatter:off
+
+var log                 = require('../../debug/Log').getInstance();
+var CoreObject          = require('./../CoreObject');
+
+//@formatter:on
+
+CoreObject.extend( CoreEvent );
+
+/**
+ * Creates a new CoreEvent
+ * @param type {string}
+ * @param opt_target {object=}
+ * @constructor
+ */
+function CoreEvent ( type, opt_target ) {
+
+    var _this = this;
+    var _type = type;
+    var _target = opt_target;
+
+    /**
+     * Sets the target of the event
+     * @param target {object}
+     */
+    _this.setTarget = function ( target ) {
+        _target = target;
+
+    }
+
+    Object.defineProperty( this, 'type', {
+        enumerable: true,
+        get: function () {
+            return _type;
+        }
+    } );
+
+    Object.defineProperty( this, 'target', {
+        enumerable: true,
+        get: function () {
+            return _target;
+        }
+    } );
+}
+
+
+module.exports = CoreEvent
