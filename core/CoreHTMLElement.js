963a97578366a43e24071d4da0a76c1aa66be794
Geert Fokke <geert@tamtam.nl>
2015-07-02 10:51:13 +0200

    Added log

--- core/CoreHTMLElement.js
+++ core/CoreHTMLElement.js
@@ -25,6 +25,7 @@ CoreEventDispatcher.extend( CoreHTMLElement );
 function CoreHTMLElement ( element ) {
 
     if( !element ) {
+        console.log(this);
         throw new Error( 'element can not be null!' );
     }
 
